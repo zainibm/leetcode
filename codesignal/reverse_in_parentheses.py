6f2eb508b78e23744a6456b679477648aed3e5e5
Zainib Mohammad <zainibm107@gmail.com>
2025-04-15 19:34:50 -0400

    updated reverse in parentheses

--- codesignal/reverse_in_parentheses.py
+++ codesignal/reverse_in_parentheses.py
@@ -1,15 +1,22 @@
 '''
 Reverse in Parentheses
+https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses
 '''
 def reverseInParentheses(s):
 	stack = []
 	res = ""
 	for c in s:
 		if c == ")":
-			temp = stack.pop()
-			while temp != "(":
+			check = stack.pop()
+			temp = ""
+			while check != "(":
+				temp += check
+				check = stack.pop()
+			if stack:
+				for t in temp:
+					stack.append(t)
+			else:
 				res += temp
-				temp = stack.pop()
 		elif c == "(" or stack:
 			stack.append(c)
 		else:
@@ -18,3 +25,4 @@ def reverseInParentheses(s):
 
 print(reverseInParentheses("(bar)")) # rab
 print(reverseInParentheses("foo(bar)baz(blim)")) # foorabbazmilb
+print(reverseInParentheses("foo(bar(baz))blim")) # foobazrabblim
